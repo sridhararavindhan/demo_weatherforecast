9823aef917860b0bf81f88f19a5229cde1801fdf
sridhararavindhan <sridhar99aravindhan@gmail.com>
2024-11-05 21:37:07 +0530

    Updates

---
+++ precache-manifest.49a92a8e52e97dc776270c531d85be13.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2655c4172a2762ecadf53ae887bdc507",
+    "url": "/demo_weatherforecast/index.html"
+  },
+  {
+    "revision": "32364c079fcde0db3285",
+    "url": "/demo_weatherforecast/static/css/main.3d7defd1.chunk.css"
+  },
+  {
+    "revision": "0e766dcbc0ef84100d27",
+    "url": "/demo_weatherforecast/static/js/2.c73606a8.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/demo_weatherforecast/static/js/2.c73606a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32364c079fcde0db3285",
+    "url": "/demo_weatherforecast/static/js/main.ed60951b.chunk.js"
+  },
+  {
+    "revision": "715bd3d8abc624abbf4b",
+    "url": "/demo_weatherforecast/static/js/runtime-main.dc241b0c.js"
+  },
+  {
+    "revision": "3077681ea260cb778fff96d874f3837e",
+    "url": "/demo_weatherforecast/static/media/01d.3077681e.svg"
+  },
+  {
+    "revision": "3077681ea260cb778fff96d874f3837e",
+    "url": "/demo_weatherforecast/static/media/01n.3077681e.svg"
+  },
+  {
+    "revision": "144d9a4769f4ff07762b2df6675faf76",
+    "url": "/demo_weatherforecast/static/media/02d.144d9a47.svg"
+  },
+  {
+    "revision": "144d9a4769f4ff07762b2df6675faf76",
+    "url": "/demo_weatherforecast/static/media/02n.144d9a47.svg"
+  },
+  {
+    "revision": "8d2c0b2d8b52984df5905ff4a8757bfb",
+    "url": "/demo_weatherforecast/static/media/03d.8d2c0b2d.svg"
+  },
+  {
+    "revision": "8d2c0b2d8b52984df5905ff4a8757bfb",
+    "url": "/demo_weatherforecast/static/media/03n.8d2c0b2d.svg"
+  },
+  {
+    "revision": "5a81a6764e4d697fcd0e0572724174cb",
+    "url": "/demo_weatherforecast/static/media/04d.5a81a676.svg"
+  },
+  {
+    "revision": "5a81a6764e4d697fcd0e0572724174cb",
+    "url": "/demo_weatherforecast/static/media/04n.5a81a676.svg"
+  },
+  {
+    "revision": "a92066b1120ede36c2eecca3da22c38b",
+    "url": "/demo_weatherforecast/static/media/09d.a92066b1.svg"
+  },
+  {
+    "revision": "a92066b1120ede36c2eecca3da22c38b",
+    "url": "/demo_weatherforecast/static/media/09n.a92066b1.svg"
+  },
+  {
+    "revision": "810f7538f29899440a31573cf8ed8501",
+    "url": "/demo_weatherforecast/static/media/10d.810f7538.svg"
+  },
+  {
+    "revision": "810f7538f29899440a31573cf8ed8501",
+    "url": "/demo_weatherforecast/static/media/10n.810f7538.svg"
+  },
+  {
+    "revision": "0fc0affe8c178150a243e62fe45e60c2",
+    "url": "/demo_weatherforecast/static/media/11d.0fc0affe.svg"
+  },
+  {
+    "revision": "0fc0affe8c178150a243e62fe45e60c2",
+    "url": "/demo_weatherforecast/static/media/11n.0fc0affe.svg"
+  },
+  {
+    "revision": "dc58368f3f03881d8a83db0acee8daa0",
+    "url": "/demo_weatherforecast/static/media/13d.dc58368f.svg"
+  },
+  {
+    "revision": "dc58368f3f03881d8a83db0acee8daa0",
+    "url": "/demo_weatherforecast/static/media/13n.dc58368f.svg"
+  },
+  {
+    "revision": "1186186b80a5955ab64b11ce34c975cf",
+    "url": "/demo_weatherforecast/static/media/50d.1186186b.svg"
+  },
+  {
+    "revision": "1186186b80a5955ab64b11ce34c975cf",
+    "url": "/demo_weatherforecast/static/media/50n.1186186b.svg"
+  },
+  {
+    "revision": "bb51f7930bcd0c21b5e6de58ae2005a6",
+    "url": "/demo_weatherforecast/static/media/background.bb51f793.jpg"
+  },
+  {
+    "revision": "961673a4bf41f578a34502a91718adf0",
+    "url": "/demo_weatherforecast/static/media/cloudy_moon.961673a4.svg"
+  }
+]);
